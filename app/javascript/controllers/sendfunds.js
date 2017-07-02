2b85d804eb04ad1c69685b9b147c7b92de2ad43b
Faeez Shaikh <faeez.shaikh@gmail.com>
2017-07-01 21:33:40 -0500

    Added modal

--- app/javascript/controllers/sendfunds.js
+++ app/javascript/controllers/sendfunds.js
@@ -2,7 +2,7 @@
 var app = angular.module("mySimpleWalletDapp");
 
 
-app.controller('SendfundsController', function($scope){
+app.controller('SendfundsController', function($scope,$ionicModal){
 
     $scope.accounts = web3.eth.accounts;
 
@@ -39,18 +39,34 @@ app.controller('SendfundsController', function($scope){
 
 
 
-    $scope.friends = [          {id:1, name:'John', phone:'555-1276',gems:5,miles:0.5,img:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5duF2C4wJfJfEoCTbe23miXSwN7De5wHwY5PtGs3KoP_aASn4Gg'},
-                         {id:2, name:'Mary', phone:'800-BIG-MARY',gems:3,miles:1.3,img:'http://www.saga.co.uk/contentlibrary/saga/publishing/verticals/money/spending/consumer-rights/parcel-scam-shutterstock-291281267.jpg'},
-                         {id:3, name:'Mike', phone:'555-4321',gems:2,miles:2.5,img:'http://www.shipping-usa.co.uk/images/send-parcel-to-usa.png'},
-                         {id:4, name:'Adam', phone:'555-5678',gems:9,miles:2.8,img:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8JtPHRxj4dx5VjPEDxq3nIeJUjwCiiusazV9_ImTVXZNpOGz4tg'},
-                         {id:5, name:'Julie', phone:'555-8765', gems:15,miles:3.0,img:'https://www.directcouriersolutions.com/wp-content/uploads/2015/02/parcel-500-660-500x600.jpg'},
-                         {id:6, name:'Juliette', phone:'555-5678', gems:3,miles:3.5,img:'http://www.book-cycle.org/wp-content/uploads/2015/08/parcel.png'},
-                          {id:7, name:'John', phone:'555-1276', gems:7,miles:4.0,img:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5duF2C4wJfJfEoCTbe23miXSwN7De5wHwY5PtGs3KoP_aASn4Gg'},
-                         {id:8, name:'Mary', phone:'800-BIG-MARY', gems:2,miles:4.5,img:'http://www.saga.co.uk/contentlibrary/saga/publishing/verticals/money/spending/consumer-rights/parcel-scam-shutterstock-291281267.jpg'},
-                         {id:9, name:'Mike', phone:'555-4321', gems:4,miles:5.0,img:'http://www.shipping-usa.co.uk/images/send-parcel-to-usa.png'},
-                         {id:10, name:'Adam', phone:'555-5678',gems:5,miles:5.5,img:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8JtPHRxj4dx5VjPEDxq3nIeJUjwCiiusazV9_ImTVXZNpOGz4tg'},
-                         {id:11, name:'Julie', phone:'555-8765', gems:6,miles:5.8,img:'https://www.directcouriersolutions.com/wp-content/uploads/2015/02/parcel-500-660-500x600.jpg'},
-                         {id:12, name:'Juliette', phone:'555-5678', gems:8,miles:6.0,img:'http://www.book-cycle.org/wp-content/uploads/2015/08/parcel.png'}];
+    $scope.friends = [{ id: 1, name: 'John', phone: '555-1276', gems: 5, miles: 0.5, img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5duF2C4wJfJfEoCTbe23miXSwN7De5wHwY5PtGs3KoP_aASn4Gg' },
+    { id: 2, name: 'Mary', phone: '800-BIG-MARY', gems: 3, miles: 1.3, img: 'http://www.saga.co.uk/contentlibrary/saga/publishing/verticals/money/spending/consumer-rights/parcel-scam-shutterstock-291281267.jpg' },
+    { id: 3, name: 'Mike', phone: '555-4321', gems: 2, miles: 2.5, img: 'http://www.shipping-usa.co.uk/images/send-parcel-to-usa.png' },
+    { id: 4, name: 'Adam', phone: '555-5678', gems: 9, miles: 2.8, img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8JtPHRxj4dx5VjPEDxq3nIeJUjwCiiusazV9_ImTVXZNpOGz4tg' },
+    { id: 5, name: 'Julie', phone: '555-8765', gems: 15, miles: 3.0, img: 'https://www.directcouriersolutions.com/wp-content/uploads/2015/02/parcel-500-660-500x600.jpg' },
+    { id: 6, name: 'Juliette', phone: '555-5678', gems: 3, miles: 3.5, img: 'http://www.book-cycle.org/wp-content/uploads/2015/08/parcel.png' },
+    { id: 7, name: 'John', phone: '555-1276', gems: 7, miles: 4.0, img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5duF2C4wJfJfEoCTbe23miXSwN7De5wHwY5PtGs3KoP_aASn4Gg' },
+    { id: 8, name: 'Mary', phone: '800-BIG-MARY', gems: 2, miles: 4.5, img: 'http://www.saga.co.uk/contentlibrary/saga/publishing/verticals/money/spending/consumer-rights/parcel-scam-shutterstock-291281267.jpg' },
+    { id: 9, name: 'Mike', phone: '555-4321', gems: 4, miles: 5.0, img: 'http://www.shipping-usa.co.uk/images/send-parcel-to-usa.png' },
+    { id: 10, name: 'Adam', phone: '555-5678', gems: 5, miles: 5.5, img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8JtPHRxj4dx5VjPEDxq3nIeJUjwCiiusazV9_ImTVXZNpOGz4tg' },
+    { id: 11, name: 'Julie', phone: '555-8765', gems: 6, miles: 5.8, img: 'https://www.directcouriersolutions.com/wp-content/uploads/2015/02/parcel-500-660-500x600.jpg' },
+    { id: 12, name: 'Juliette', phone: '555-5678', gems: 8, miles: 6.0, img: 'http://www.book-cycle.org/wp-content/uploads/2015/08/parcel.png' }];
 
 
+
+
+
+    $ionicModal.fromTemplateUrl('views/modal.html', {
+        scope: $scope,
+        animation: 'slide-in-up'
+    }).then(function (modal) {
+        $scope.modal = modal;
+    });
+    $scope.openComposer = function () {
+        console.log('Opening Modal');
+        $scope.modal.show();
+    };
+    $scope.closeModal = function () {
+        $scope.modal.hide();
+    };
 });
