df4a679a1b9e56a3c180d969b7a35061618e2c79
Faeez Shaikh <faeez.shaikh@gmail.com>
2017-07-04 23:08:49 -0500

    Refactored

--- app/javascript/controllers/main.js
+++ app/javascript/controllers/main.js
@@ -18,11 +18,4 @@ app.controller("MainController", function ($scope) {
         });
     });
 
-
-// Without JQuery
-// var slider = new Slider("#ex6");
-// slider.on("slide", function(sliderValue) {
-// 	document.getElementById("ex6SliderVal").textContent = sliderValue;
-// });
-
 });
