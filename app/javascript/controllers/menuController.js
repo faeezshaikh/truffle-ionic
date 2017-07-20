876a3d9a23f59f5a1d6d11455ff7da38ae53855e
Faeez Shaikh <faeez.shaikh@gmail.com>
2017-07-20 00:40:07 -0500

    Funding smart contract from other accts

--- app/javascript/controllers/menuController.js
+++ app/javascript/controllers/menuController.js
@@ -4,10 +4,21 @@ var app = angular.module("blockchainExpressDapp");
 
 
 app.controller('MenuController', function ($scope, $ionicModal, $ionicScrollDelegate,DappService) {
-     BlockexGem.deployed().then(function(contract) {
+     BlockexGem.deployed().then(function(instance) {
 
+        web3.eth.defaultAccount = web3.eth.accounts[0];
+        var contract = instance;
         // For some reason contract.address for smart contract address is giving issues. Hence designating account 3 as escrow (smart contract) address
         // var contract_address = contract.address;
+        // var contract_address = contract.address;
+
+
+        web3.eth.sendTransaction({from: web3.eth.accounts[4], to: web3.eth.accounts[3] , value: web3.toWei(99, "ether")});
+        web3.eth.sendTransaction({from: web3.eth.accounts[5], to: web3.eth.accounts[3] , value: web3.toWei(99, "ether")});
+        web3.eth.sendTransaction({from: web3.eth.accounts[6], to: web3.eth.accounts[3] , value: web3.toWei(99, "ether")});
+        web3.eth.sendTransaction({from: web3.eth.accounts[7], to: web3.eth.accounts[3] , value: web3.toWei(99, "ether")});
+        web3.eth.sendTransaction({from: web3.eth.accounts[8], to: web3.eth.accounts[3] , value: web3.toWei(99, "ether")});
+        web3.eth.sendTransaction({from: web3.eth.accounts[9], to: web3.eth.accounts[3] , value: web3.toWei(5, "ether")});
         var contract_address = web3.eth.accounts[3];
         console.log('Contract Address: ',  contract_address);
 
@@ -18,8 +29,16 @@ app.controller('MenuController', function ($scope, $ionicModal, $ionicScrollDele
         
 
         getBlockchainAddressBalance(contract_address,'Smart Contract');
-        // contract.setBalance(contract_address,0);
-        getBlockchainAddressBalance(contract_address,'Smart Contract');
+        // contract.setBalance(contract_address,5).then(function(result) {
+        //     console.log('Returning after setting contract balance', result);
+        //     getBlockchainAddressBalance(contract_address,'Smart Contract');
+        // });
+
+
+
+
+        // Printing Balance for Person0 (100 Eth)
+        getBlockchainAddressBalance(web3.eth.accounts[0],'Person 0');
 
 
         // Set Balance for Person1  (100 Eth)
@@ -42,6 +61,7 @@ app.controller('MenuController', function ($scope, $ionicModal, $ionicScrollDele
             // });
            
     });
+
 
      function getBlockchainAddressBalance(address,nickname){
          var val = web3.eth.getBalance(address);
